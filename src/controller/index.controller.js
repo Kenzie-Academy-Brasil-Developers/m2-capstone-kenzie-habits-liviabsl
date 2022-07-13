7018a1f884873851510b8a1d537c2004a8baf6e9
Sidarta Kauã <odssy351@gmail.com>
2022-07-13 17:38:05 -0500

    fix: consertando mensagem de erro

--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -3,11 +3,12 @@ import LoginResquest from "./login.controller.js";
 export default class LoginDOM {
   static getLogin() {
     const botaoEntrar = document.querySelector(".btn_getIn");
+    const inputsLogin = document.querySelectorAll(".inputLogin");
+    const spansLogin = document.querySelectorAll(".spanError");
 
     botaoEntrar.addEventListener("click", async (e) => {
       e.preventDefault();
       const data = {};
-
       const inputs = [...e.srcElement.form];
       inputs.forEach((input) => {
         if (input.value !== "") {
@@ -19,9 +20,22 @@ export default class LoginDOM {
       const userToken = JSON.parse(localStorage.getItem("@kenzie-habit:token"));
       if (userToken !== null) {
         window.location.href = "./src/views/homepage.index.html";
+      } else {
+        if (inputs[0].value == "") {
+          inputsLogin[0].classList.add("inputError");
+          spansLogin[0].classList.remove("none");
+        } else {
+          inputsLogin[0].classList.remove("inputError");
+          spansLogin[0].classList.add("none");
+        }
+        if (inputs[1].value == "") {
+          inputsLogin[1].classList.add("inputError");
+          spansLogin[1].classList.remove("none");
+        } else {
+          inputsLogin[1].classList.remove("inputError");
+          spansLogin[1].classList.add("none");
+        }
       }
     });
   }
 }
-
-LoginDOM.getLogin();
